600b90113563af009a2e6cc5855ebc9acc7aa111
Reanna Ishmael <rishmael@fordham.edu>
2022-07-20 21:55:10 -0400

    Add Header to app and change background color

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Header from "./components/Header";
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>Testing, Testing</h1>
+      <Header />
     </div>
   );
 }
