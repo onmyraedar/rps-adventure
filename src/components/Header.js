90d09217242e308856dc59d63b3e104dbefdbc97
Reanna Ishmael <rishmael@fordham.edu>
2022-07-20 22:55:06 -0400

    Convert Header to functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Header.css";
 
-class Header extends Component {
-    render() {
-        return(
-            <div className="header">
-                <p className="app-title">Quark</p>
-                <p className="app-description">A rock-paper-scissors gachaventure.</p>
-            </div>
-        );
-    }
+function Header() {
+    return(
+        <div className="header">
+            <p className="app-title">Quark</p>
+            <p className="app-description">A rock-paper-scissors gachaventure.</p>
+        </div>
+    );
 }
 
 export default Header;
