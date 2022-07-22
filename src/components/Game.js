137d3df52876e9e055c22259f7ce25678ec96dba
Reanna Ishmael <rishmael@fordham.edu>
2022-07-21 21:24:18 -0400

    Create unique keys for cards
    When rendering the player's card inventory, we need to add a unique key to each element.
    
    Use uniqid to generate a unique key for each card.
    
    Also, save the ID of the last played card in state. This card will be used in gameplay functions.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import uniqid from "uniqid";
 import "./Game.css";
 
 function Game() {
     const [coins, setCoins] = useState(0);
     const [inventory, setInventory] = useState([
         { 
+            id: uniqid(),
             name: "Pebble",
             type: "Rock",
             strongAgainst: "Scissors",
@@ -12,6 +14,7 @@ function Game() {
             winChance: 100 
         },
         { 
+            id: uniqid(),
             name: "Looseleaf",
             type: "Paper",
             strongAgainst: "Rock",
@@ -19,6 +22,7 @@ function Game() {
             winChance: 100 
         },
         {
+            id: uniqid(),
             name: "Blunt Tip",
             type: "Scissors",
             strongAgainst: "Paper",
@@ -26,23 +30,38 @@ function Game() {
             winChance: 100 
         }
     ]);
+    const [gameData, setGameData] = useState({
+        round: 0,
+        userCardID: ""
+    });
 
     return(
         <div className="game-container">
-            <h3>Coins: {coins}</h3>
-            <h3>Your Cards</h3>
-            <div className="inventory">
-                {inventory.map((card) => (
-                    <div className="card-container">
-                        <p className="card-name">
-                            <b>{card.name}</b>
-                        </p>
-                        <div className="card-type-badge">
-                            <p>{card.type}</p>
+            <div className="game-container-left">
+                <h3>Coins: {coins}</h3>
+                <h3>Your Cards</h3>
+                <div className="inventory">
+                    {inventory.map((card) => (
+                        <div key={card.id} className="card-container">
+                            <p className="card-name">
+                                <b>{card.name}</b>
+                            </p>
+                            <div className="card-type-badge">
+                                <p>{card.type}</p>
+                            </div>
+                            <p>{card.winChance}% win chance against {card.weakAgainst} type cards</p>
+                            <button value={card.id} onClick={(e) => setGameData({
+                                ...gameData,
+                                userCardID: e.target.value
+                            })}>Play this card</button>
                         </div>
-                        <p>{card.winChance}% win chance against {card.weakAgainst} type cards</p>
-                    </div>
-                ))}
+                    ))}
+                </div>
+            </div>
+            <div className="game-container-right">
+                <h1>Play a card.</h1>
+                <h3>Round: {gameData.round}</h3>
+                <h3>{gameData.userCardID}</h3>
             </div>
         </div>
     );
