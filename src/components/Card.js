f7473b5f6d2eab756b474fe375e81a0726a66073
Reanna Ishmael <rishmael@fordham.edu>
2022-07-22 14:32:02 -0400

    Create a Card component
    As the Game component grows larger, the code suffers from a lack of readability.
    
    Fix this by creating a Card subcomponent.
    
    Card is reused within Game, which improves readability and allows for fewer lines of HTML.

---
+++ src/components/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Card(props) {
+    const { card, playable, onPlay } = props;
+
+    let playButton;
+    if (playable) {
+        playButton = <button value={card.id} onClick={onPlay}>Play this card</button>
+    }
+
+    return(
+        <div className="card-container">
+            <p className="card-name">
+                <b>{card.name}</b>
+            </p>
+            <div className="card-type-badge">
+                <p>{card.type}</p>
+            </div>
+            <p>{card.winChance}% win chance against {card.weakAgainst} type cards</p>
+            {playButton}
+        </div>
+    );
+}
+
+export default Card;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import uniqid from "uniqid";
+import Card from "./Card.js";
 import "./Game.css";
 
 function Game() {
@@ -31,9 +32,20 @@ function Game() {
         }
     ]);
     const [gameData, setGameData] = useState({
-        round: 0,
+        round: 1,
         activeCard: {}
     });
+    
+    function savePlayedCard(e) {
+        const lastPlayedCard = inventory.find(
+            (card) => card.id === e.target.value
+        );
+        console.log(lastPlayedCard.id);
+        setGameData({
+            ...gameData,
+            activeCard: lastPlayedCard
+        });      
+    } 
 
     return(
         <div className="game-container">
@@ -42,26 +54,7 @@ function Game() {
                 <h3>Your Cards</h3>
                 <div className="inventory">
                     {inventory.map((card) => (
-                        <div key={card.id} className="card-container">
-                            <p className="card-name">
-                                <b>{card.name}</b>
-                            </p>
-                            <div className="card-type-badge">
-                                <p>{card.type}</p>
-                            </div>
-                            <p>{card.winChance}% win chance against {card.weakAgainst} type cards</p>
-                            <button value={card.id} onClick={(e) => {
-                                const filteredInventory = inventory.filter(
-                                    card => card.id === e.target.value
-                                );
-                                const lastPlayedCard = filteredInventory[0];
-                                console.log(lastPlayedCard);
-                                setGameData({
-                                    ...gameData,
-                                    activeCard: lastPlayedCard
-                                });
-                            }}>Play this card</button>
-                        </div>
+                        <Card key={card.id} card={card} playable={true} onPlay={savePlayedCard}/>
                     ))}
                 </div>
             </div>
@@ -69,15 +62,7 @@ function Game() {
                 <h1>Play a card.</h1>
                 <h3>Round: {gameData.round}</h3>
                 <h3>You just played this card:</h3>
-                <div key={gameData.activeCard.id} className="card-container">
-                    <p className="card-name">
-                        <b>{gameData.activeCard.name}</b>
-                    </p>
-                    <div className="card-type-badge">
-                        <p>{gameData.activeCard.type}</p>
-                    </div>
-                    <p>{gameData.activeCard.winChance}% win chance against {gameData.activeCard.weakAgainst} type cards</p>
-                </div>
+                <Card key={gameData.activeCard.id} card={gameData.activeCard} playable={false} onPlay={savePlayedCard}/> 
             </div>
         </div>
     );
